397647fa9eb63d26ffe90d8bfd87d64cf884a6f4
camaran <camaran@gmail.com>
2015-12-10 14:58:29 +0100

    Child Theme in PHP Class

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php
-  add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
-  function enqueue_parent_theme_style() {
-  wp_enqueue_style( 'pasw2015_parent_style', get_template_directory_uri().'/style.css' );
+class pasw2015_child_theme {
+
+    static $instance = false;
+        
+    public function __construct() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_parent_theme_style') );
+    }
+
+    public static function getInstance() {
+		  if ( !self::$instance )
+			  self::$instance = new self;
+		  return self::$instance;
+	  }
+ 
+    function enqueue_parent_theme_style()  {
+        wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    }
 }
+$pasw2015_child_theme = pasw2015_child_theme::getInstance();
+?>
